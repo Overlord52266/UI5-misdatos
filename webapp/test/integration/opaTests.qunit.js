0862b35afe2a42232c7b0683bf4b41d068e82e73
SEBASTIAN CASTILLO <scastillo@ravaconsulting.com.pe>
2025-03-06 14:38:04 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["kpmg/datos/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"kpmg.datos/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
